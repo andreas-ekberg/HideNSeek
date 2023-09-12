ea7acec228c822c07dcbc9f53d276d2a67aff312
Luckelina <93657078+Luckelina@users.noreply.github.com>
2023-09-12 12:24:13 +0200

    första commit med grejer

---
+++ Assets/Scripts/Behaaaave tree.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaaaavetree : MonoBehaviour
+{
+    public GameObject Chase;
+    public GameObject Hide;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
